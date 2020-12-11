cfe6309926107812a076f3548dcde847d7e21813
Josh Lauer <jlauer@doximity.com>
2020-12-10 22:24:22 -0500

    solution day 1

---
+++ days/1/run.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+INPUT_FILE_PATH = File.join(File.dirname(__FILE__), "input")
+
+numbers = IO.readlines(INPUT_FILE_PATH).map(&:to_i)
+pair = numbers.combination(2).detect { |c| c.sum == 2020 }
+puts pair.inject(:*)
