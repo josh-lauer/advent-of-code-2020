93139e28b9e1b249a5c1121fa1947f0fe9e7ef41
Josh Lauer <jlauer@doximity.com>
2020-12-10 22:41:22 -0500

    better term

--- days/1/part_2.rb
+++ days/1/part_2.rb
@@ -3,5 +3,5 @@
 INPUT_FILE_PATH = File.join(File.dirname(__FILE__), "input")
 
 numbers = IO.readlines(INPUT_FILE_PATH).map(&:to_i)
-pair = numbers.combination(3).detect { |c| c.sum == 2020 }
-puts pair.inject(:*)
+tuple = numbers.combination(3).detect { |c| c.sum == 2020 }
+puts tuple.inject(:*)
