361e9984ddedd73ee635ca2bf7a34d633675ba43
Josh Lauer <jlauer@doximity.com>
2020-12-12 01:08:04 -0500

    day 2, part 2

--- days/2/part_1.rb
+++ days/2/part_1.rb
@@ -2,10 +2,10 @@
 
 INPUT_FILE_PATH = File.join(File.dirname(__FILE__), "input")
 
-input = IO.readlines(INPUT_FILE_PATH).map(&:chomp)
+input = IO.readlines(INPUT_FILE_PATH, chomp: true)
 
 valid_passwords = input.map { |i| i.split(' ') }.select do |policy_range, policy_char, password|
-  Range.new(*policy_range.split('-').map(&:to_i)).include?(password.split('').count(policy_char[0]))
+  Range.new(*policy_range.split('-').map(&:to_i)).include?(password.chars.count(policy_char[0]))
 end
 
 puts valid_passwords.size
---
+++ days/2/part_2.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+INPUT_FILE_PATH = File.join(File.dirname(__FILE__), "input")
+
+input = IO.readlines(INPUT_FILE_PATH, chomp: true).map(&:chomp)
+
+valid_passwords = input.map { |i| i.split(' ') }.select do |positions, policy_char, password|
+  password.chars.values_at(*positions.split('-').map{|n| n.to_i.pred}).count(policy_char[0]) == 1
+end
+
+puts valid_passwords.size
