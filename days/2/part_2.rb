9cdcb510a0d69ad6f07b3ed808fa951a70e82f3a
Josh Lauer <jlauer@doximity.com>
2020-12-13 13:03:18 -0500

    simpler

--- days/2/part_2.rb
+++ days/2/part_2.rb
@@ -5,7 +5,7 @@ INPUT_FILE_PATH = File.join(File.dirname(__FILE__), "input")
 input = IO.readlines(INPUT_FILE_PATH, chomp: true).map(&:chomp)
 
 valid_passwords = input.map { |i| i.split(' ') }.select do |positions, policy_char, password|
-  password.chars.values_at(*positions.split('-').map{|n| n.to_i.pred}).count(policy_char[0]) == 1
+  password.chars.values_at(*positions.split('-').map { |n| n.to_i - 1 }).count(policy_char[0]) == 1
 end
 
 puts valid_passwords.size
